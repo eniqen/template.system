4de9dd48f7ad43c2d9d67b6a8fc461395ef27cca
eniqen <r2n@bk.ru>
2017-03-05 19:47:01 +0300

    чилд роутинг

--- src/main/webapp/app/app.routing.module.ts
+++ src/main/webapp/app/app.routing.module.ts
@@ -1,14 +1,19 @@
-
 import {RouterModule, Routes} from "@angular/router";
 import {DocumentComponent} from "./components/document/document.component";
 import {TemplateComponent} from "./components/template/template.component";
 import {TemplateService} from "./services/template.service";
 import {DocumentService} from "./services/document.service";
+import {TemplateFieldComponent} from "./components/template/template.field.component";
 
 export const routes: Routes = [
-    { path: '', redirectTo: 'templates', pathMatch: 'full' },
-    { path: 'templates',  component: TemplateComponent },
-    { path: 'documents', component: DocumentComponent }
+    {path: '', redirectTo: 'templates', pathMatch: 'full'},
+    {
+        path: 'templates', component: TemplateComponent,
+        children: [
+            {path: 'test', component: TemplateFieldComponent},
+        ]
+    },
+    {path: 'documents', component: DocumentComponent}
 ];
 
 export const appRoutingProviders: any[] = [
